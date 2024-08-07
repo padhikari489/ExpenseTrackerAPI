40e5bdb2c959918b59033a84e19a49acfc717bdc
Pawan Raj Adhikari <pawan.adhikari2336@gmail.com>
2024-08-06 23:46:25 -0500

    added Http status codes

--- src/main/java/com/myproject/expensetrackerapi/controller/ExpenseController.java
+++ src/main/java/com/myproject/expensetrackerapi/controller/ExpenseController.java
@@ -3,12 +3,14 @@ package com.myproject.expensetrackerapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myproject.expensetrackerapi.entity.Expense;
@@ -19,27 +21,30 @@ public class ExpenseController {
 
 	@Autowired
 	private ExpenseService expenseService;
-	
+
 	@GetMapping("/expenses")
 	public List<Expense> getAllExpenses() {
 		List<Expense> ret = expenseService.getAllExpenses();
 		return ret;
 	}
-	
+
 	@GetMapping("/expenses/{id}")
+	@ResponseStatus(value = HttpStatus.OK)
 	public Expense getExpenseById(@PathVariable Long id) {
 		return expenseService.getExpenseById(id);
-		
+
 	}
-	
+
 	@DeleteMapping("/expenses")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void deleteExpenseById(@RequestParam Long id) {
 		expenseService.deleteExpenseById(id);
 	}
-	
-	 @PostMapping("/expenses")
-	    public Expense saveExpense(@RequestBody Expense expense) {
-	        return expenseService.saveExpense(expense);
-	    }
+
+	@PostMapping("/expenses")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Expense saveExpense(@RequestBody Expense expense) {
+		return expenseService.saveExpense(expense);
+	}
 
 }
